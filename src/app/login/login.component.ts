006209d58a02f8b0decf62293564c6370fb2867c
Richard Lopez <richardsamuellopez@gmail.com>
2022-10-10 20:21:59 -0400

    More styles

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public loginForm!: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      token: new FormControl('', Validators.required),
+    });
+  }
+
+  public onSubmit() {
+    // this.authenticationService.login(
+    //   this.loginForm.get('username')!.value,
+    //   this.loginForm!.get('password')!.value
+    // );
   }
 
 }
